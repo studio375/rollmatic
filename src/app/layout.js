42e8ae7ae642d15a5ffa352cce8c8238618506f2
cosimo375 <cosimo@studio375.it>
2026-04-20 18:06:47 +0200

    installed font

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,40 +4,22 @@ import SmoothScroll from "@/components/Utility/SmoothScroll";
 import localFont from "next/font/local";
 import Footer from "@/components/Library/Footer/footer";
 
-const ppNeue = localFont({
+
+const aspekta = localFont({
   src: [
     {
-      path: "../assets/fonts/PPNeueMontreal-Book.woff",
-      weight: "400",
+      path: "../assets/fonts/AspektaVF.woff2",
       style: "normal",
     },
   ],
-  variable: "--font-neue",
-  weight: "400",
+  variable: "--font-aspekta",
+  weight: "100 900",
 });
-
-const ppHatton = localFont({
-  src: [
-    {
-      path: "../assets/fonts/PPHatton-Ultralight.woff",
-      weight: "300",
-      style: "normal",
-    },
-  ],
-  variable: "--font-hatton",
-  weight: "300",
-});
-
-export const metadata = {
-  title: "Next375 - A Next.js Starter Kit",
-  description:
-    "A Next.js starter kit for building modern web applications, by Studio375.",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="it-IT">
-      <body className={`${ppNeue.variable} ${ppHatton.variable} antialiased`}>
+      <body className={`${aspekta.variable} antialiased`}>
         <Header />
         <SmoothScroll>{children}</SmoothScroll>
         <Footer />
